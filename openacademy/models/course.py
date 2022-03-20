d0db4e4cde4a3f693759f68a145dd6d41c309fcc
Oleksandr Glotov <ag@wwind.ua>
2022-03-20 16:31:27 +0000

    constraints sql

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -11,6 +11,10 @@ class Course(models.Model):
     description = fields.Text(string="Description")
     responsible_id = fields.Many2one(comodel_name="res.users", string="Responsible", required=False, )
     session_id = fields.Many2one(comodel_name="openacademy.session", string="Session", required=False, )
+    _sql_constraints = [
+        ('title_uniq', 'unique (title)', "Course title already exists !"),
+        ('title_notequal_descr', 'check (title <> description)', "Title and description should be different !"),
+    ]
 
 #     @api.depends('value')
 #     def _value_pc(self):
