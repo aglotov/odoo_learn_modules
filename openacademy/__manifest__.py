e291577ecaddfd1535f6e185800a90a1185d9e42
Oleksandr Glotov <ag@wwind.ua>
2022-03-23 16:19:09 +0000

    Dashboard

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '15.0.1.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
     'application': True,
 
     # always loaded
@@ -29,6 +29,7 @@
         'views/course_views.xml',
         'views/session_report.xml',
         'views/report_session.xml',
+        'views/dashboard.xml',
         'wizard/create_session_view.xml',
         'data/course_data.xml',
     ],
