41a3cd7de5066b361a364b75aece75b5293aec3a
Oleksandr Glotov <ag@wwind.ua>
2022-03-24 20:57:05 +0000

    3.4 Templates

---
+++ academy/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy",
+
+    'summary': """
+        Academy: Website Test Module""",
+
+    'description': """
+        This module is designed to test website creation
+    """,
+
+    'author': "Oleksandr Glotov",
+    'website': "http://wwind.ua",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Website',
+    'version': '15.0.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application': True,
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
